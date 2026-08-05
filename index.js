6b56f1627aba0d165f2dc80ace6c78b206613a17
João Pedro <TocooXD@gmail.com>
2026-08-04 23:12:19 -0300

    feat: adiciona o projeto de API rest crud

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+const cursos = [`Node.js`, `JavaScript`, `React Native`];
+
+server.get(`/cursos`, (req, res) => {
+
+    return res.json(cursos);
+});
+
+server.get(`/cursos/:index`, (req, res) => {
+
+    const {index} = req.params;
+    return res.json(cursos[index]);
+});
+
+server.post(`/cursos`, (req, res) => {
+
+    const {name} = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+server.put(`/cursos/:index`, (req, res) => {
+
+    const {index} = req.params;
+    const {name} = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
+server.delete(`/cursos/:index`, (req, res) => {
+
+    const {index} = req.params;
+
+    cursos.splice(index, 1);
+
+    return res.send("Curso deletado com sucesso!")
+});
+
+server.listen(3000);
